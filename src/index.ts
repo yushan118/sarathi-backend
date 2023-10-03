bbe39e7d28256a821875bba75f29c3b84da11fc8
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-10-03 10:15:00 +0545

    Initialized mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
@@ -19,6 +21,12 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080/");
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
