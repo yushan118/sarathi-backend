ece63196e993200db70bf6534a3f8887cd299c50
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-12-19 14:20:00 +0545

    Added total assigned/completed count

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -10,12 +10,20 @@ export async function dashboard(
   try {
     const bookingsCount = await BookingModel.countDocuments();
     const usersCount = await UserModel.countDocuments();
+    const assignedCount = await BookingModel.countDocuments({
+      status: { $ne: "Waiting to be approved by admin" },
+    });
+    const completedCount = await BookingModel.countDocuments({
+      status: { $ne: "Arrived on hospital" },
+    });
 
     return res
       .status(200)
       .json({
         bookings_count: bookingsCount,
         users_count: usersCount,
+        assigned_count: assignedCount,
+        completed_count: completedCount,
       })
       .end();
   } catch (error) {
