ea1709d19110bbb56e8c3eff8b949ee527cfd4ef
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-12-30 18:13:00 +0545

    Added user manipulation API

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { AuthenticatedRequest } from "../interfaces/request";
+import {
+  AuthenticatedAdminRequest,
+  AuthenticatedRequest,
+} from "../interfaces/request";
+import { UserModel } from "../db/users";
 
 export async function currentUser(
   req: AuthenticatedRequest,
@@ -7,6 +11,36 @@ export async function currentUser(
 ) {
   try {
     return res.json(req.user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
+export async function getAllUsers(
+  _req: AuthenticatedAdminRequest,
+  res: express.Response
+) {
+  try {
+    const users = await UserModel.find();
+    return res.status(200).json(users).end();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
+export async function removeUser(
+  req: AuthenticatedAdminRequest,
+  res: express.Response
+) {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "id is required fields" }).end();
+    }
+    await UserModel.deleteOne({ _id: id });
+    return res.status(200).json({ success: true }).end();
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: "Something went wrong" });
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { currentUser } from "../controllers/users";
-import { isAuthenticated } from "../middlewares";
+import { currentUser, getAllUsers, removeUser } from "../controllers/users";
+import { isAdminAuthenticated, isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
   router.get("/user/current", isAuthenticated, currentUser);
+  router.get("/user/list", isAdminAuthenticated, getAllUsers);
+  router.delete("/user/:id", isAdminAuthenticated, removeUser);
 };
